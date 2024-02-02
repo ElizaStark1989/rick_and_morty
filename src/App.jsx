b356df5bcd5b7cbc6b300876ea299fa1a4c5b000
Elizabeth <elizastark-1989@outlook.es>
2024-02-02 16:27:51 -0500

    Inicio de proyecto

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+  const [location, setLocation] = useState();
+
+  useEffect(() => {
+   
+  }, []);
+  
+
+  return (
+   <div>
+    <h1>Rick and Morty</h1>
+   </div>
+  )
+}
+
+export default App;
