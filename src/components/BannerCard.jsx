3996cbacbba4eade4e93ddea7b4aa29fec8fdd33
Elizabeth <elizastark-1989@outlook.es>
2024-02-06 19:22:00 -0500

    Detalles de maquetado y paginacion

---
+++ src/components/BannerCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const BannerCard = () => {
+  return (
+    <div>
+        <img className='banner_img' src = 'rick.jpg'/>
+    </div>
+  )
+}
+
+export default BannerCard;
